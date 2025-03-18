1e7f4013a55ac4458a7859fb2d2f58cf1e4fb8c4
wangk <liziheimao@163.com>
2025-03-18 23:48:12 +0800

    Initial Commit

---
+++ Idoolk/IdoolkApp.swift
@@ -0,0 +1,17 @@
+//
+//  IdoolkApp.swift
+//  Idoolk
+//
+//  Created by wang k on 2025/3/18.
+//
+
+import SwiftUI
+
+@main
+struct IdoolkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
